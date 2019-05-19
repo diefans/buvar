c730d775b642dec4b7e8ba152333408995d24de3
Oliver Berger <oliver@digitalarchitekt.de>
2019-05-19 23:38:57 +0200

    very simple config
    - merge from multiple sources
    - load sub sections on demand
    - apply environment vars

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="boovar",
+    version="0.0.1",
+    author="Oliver Berger",
+    author_email="diefans@gmail.com",
+    description="General purpose config loader",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://gitlab.com/diefans/boovar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    package_dir={'': 'src'},
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#find-namespace-packages
+    packages=setuptools.find_namespace_packages(where='src'),
+    entry_points={
+        'console_scripts': []
+    },
+    install_requires=[],
+)
---
+++ tests/test_config.py
@@ -0,0 +1,67 @@
+def test_config_source_schematize(mocker):
+    import boovar
+    import attr
+
+    @attr.s(auto_attribs=True)
+    class FooConfig:
+        bar: str = 'default'
+        foobar: float = 9.87
+        baz: bool = boovar.bool_var(default=False)
+
+    @attr.s(auto_attribs=True)
+    class BarConfig:
+        bim: float
+        foo: FooConfig = FooConfig()
+
+    @attr.s(auto_attribs=True, kw_only=True)
+    class BimConfig:
+        bar: BarConfig
+        bam: bool = boovar.bool_var()
+        bum: int = 123
+
+    sources = [
+        {
+            'bar': {
+                'bim': '123.4',
+                'foo': {
+                    'bar': '1.23',
+                    'baz': 'true',
+                },
+            },
+        }, {
+            'foo': {
+                'bar': 'value',
+                'foobar': 123.5,
+                'baz': True,
+            }
+        }, {
+            'bar': {
+                'bim': '123.4',
+            },
+            'bim': {
+                'bar': {
+                    'bim': 1.23
+                },
+                'bam': 'on'
+            },
+        }
+    ]
+
+    mocker.patch('os.environ', {
+        'PREFIX_BAR_BIM': '0',
+        'PREFIX_BAR_FOO_FOOBAR': '7.77',
+        'PREFIX_BAR_FOO_BAZ': 'false'
+    })
+
+    cfg = boovar.Config.from_sources(*sources, env_prefix='PREFIX')
+    cfg.bar = BarConfig
+    cfg.foo = FooConfig
+    cfg.bim = BimConfig
+
+    assert cfg.__dict__ == {
+        'bar': BarConfig(bim=0.0, foo=FooConfig(bar='1.23', foobar=7.77, baz=False)),
+        'foo': FooConfig(bar='value', foobar=123.5, baz=True),
+        'bim': BimConfig(bar=BarConfig(bim=1.23), bam=True),
+    }
+
+    assert cfg.foo.baz

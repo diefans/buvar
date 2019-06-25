80cc177360596893f4a26f95eb02843acd120f75
Oliver Berger <oliver@digitalarchitekt.de>
2019-06-25 16:18:24 +0200

    refactored config
    There is only ConfigSource left, which loads config schemes on demand.

--- src/buvar/config.py
+++ src/buvar/config.py
@@ -6,7 +6,7 @@ import attr
 import structlog
 import tomlkit
 
-CNF_KEY = 'buvar_config'
+CNF_KEY = "buvar_config"
 
 
 logger = structlog.get_logger()
@@ -16,60 +16,14 @@ missing = object()
 
 
 @attr.s(auto_attribs=True)
-class Config:
-
-    """Config dict, with loadable sections.
-
-    >>> @attr.s(auto_attribs=True)
-    ... class FooConfig:
-    ...     bar: float
-    >>>
-    >>>
-    >>> source = {'foo': {'bar': 1.23}}
-    >>> config = Config.from_sources(source)
-    >>> config.foo = FooConfig
-    >>> config.foo
-    FooConfig(bar=1.23)
-    """
-
-    __slots__ = ('__source', '__dict__', '__env_prefix')
-
-    @classmethod
-    def from_sources(cls, *sources, env_prefix=None):
-        __source = {}
-        for source in sources:
-            merge_dict(__source, source)
-
-        config = schematize(__source, cls, env_prefix=env_prefix)
-        config.__env_prefix = env_prefix        # noqa: W0212
-        config.__source = __source              # noqa: W0212
-        return config
-
-    def __setattr__(self, name, value):
-        """Load a source section by applying the `scheme` upon it."""
-        if hasattr(value, '__attrs_attrs__'):
-            value = self.__process_scheme(name, value)
-        super().__setattr__(name, value)
-    __setitem__ = __setattr__
-
-    def __process_scheme(self, name, scheme):
-        values = self.__source[name]
-        scheme = schematize(
-            values, scheme,
-            env_prefix='_'.join(
-                part for part in (self.__env_prefix, name.upper()) if part
-            )
-        )
-        return scheme
-
-
-@attr.s(auto_attribs=True)
 class ConfigValue:
     name: str = None
     help: str = None
 
 
-def var(default=missing, converter=None, name=None, validator=None, help=None):    # noqa: W0622
+def var(
+    default=missing, converter=None, name=None, validator=None, help=None
+):  # noqa: W0622
     return attr.ib(
         default=default,
         metadata={CNF_KEY: ConfigValue(name, help)},
@@ -85,18 +39,78 @@ def _env_to_bool(val):
     if isinstance(val, bool):
         return val
     val = val.strip().lower()
-    if val in ('1', 'true', 'yes', 'on'):
+    if val in ("1", "true", "yes", "on"):
         return True
 
     return False
 
 
-def bool_var(default=missing, name=None, help=None):   # noqa: W0622
+def _env_to_list(val):
+    """Take a comma separated string and split it."""
+    if isinstance(val, str):
+        val = map(lambda x: x.trim(), val.split(","))
+    return val
+
+
+def bool_var(default=missing, name=None, help=None):  # noqa: W0622
     return var(default=default, name=name, converter=_env_to_bool, help=help)
 
 
+def list_var(default=missing, name=None, help=None):  # noqa: W0622
+    return var(default=default, name=name, converter=_env_to_list, help=help)
+
+
+@attr.s(auto_attribs=True)
+class BuvarConfig:
+    log_level: str = var(help="The log level to set")
+    include: typing.List[str] = list_var(help="A list of plugins to include")
+
+
+class ConfigSource(dict):
+
+    """Config dict, with loadable sections.
+
+    >>> @attr.s(auto_attribs=True)
+    ... class FooConfig:
+    ...     bar: float
+    >>>
+    >>>
+    >>> source = {'foo': {'bar': 1.23}}
+    >>> config = ConfigSource(source)
+    >>> foo = config.load(FooConfig, "foo")
+    >>> foo
+    FooConfig(bar=1.23)
+    """
+
+    __slots__ = ("env_prefix",)
+
+    def __init__(self, *sources, env_prefix=None):
+        super().__init__()
+        for source in sources:
+            merge_dict(self, source)
+        # config = schematize(__source, cls, env_prefix=env_prefix)
+        self.env_prefix = env_prefix  # noqa: W0212
+
+    def load(self, scheme, name=...):
+        if name is ...:
+            values = self
+        else:
+            values = self[name]
+
+        scheme = schematize(
+            scheme,
+            values,
+            env_prefix="_".join(
+                part
+                for part in (self.env_prefix, name.upper() if name is not ... else None)
+                if part
+            ),
+        )
+        return scheme
+
+
 def isattrs(obj):
-    return hasattr(obj, '__attrs_attrs__')
+    return hasattr(obj, "__attrs_attrs__")
 
 
 def merge_dict(dest, source):
@@ -114,25 +128,21 @@ def merge_dict(dest, source):
     return dest
 
 
-def schematize(source, attrs, env_prefix=''):
+def schematize(attrs, source, env_prefix=""):
     """Create a scheme from the source dict and apply environment vars."""
     attrs_kwargs = {}
-    assert isattrs(attrs), f'{attrs} must be attrs decorated'
+    assert isattrs(attrs), f"{attrs} must be attrs decorated"
     for attrib in attrs.__attrs_attrs__:
         a_name = attrib.name
-        env_name = '_'.join(
-            part for part in (env_prefix, a_name.upper()) if part
-        )
+        env_name = "_".join(part for part in (env_prefix, a_name.upper()) if part)
         try:
             if isattrs(attrib.type):
-                a_value = schematize(source[a_name],
-                                     attrib.type,
-                                     env_prefix=env_name)
+                a_value = schematize(attrib.type, source[a_name], env_prefix=env_name)
             else:
                 a_value = os.environ.get(env_name, source.get(a_name, missing))
                 if a_value is missing:
                     if attrib.default is missing:
-                        raise ValueError('Attribute is missing', a_name, env_name)
+                        raise ValueError("Attribute is missing", a_name, env_name)
                     # we skip this value if source is lacking but we have a default
                     continue
                 if attrib.converter is None:
@@ -143,28 +153,27 @@ def schematize(source, attrs, env_prefix=''):
                         a_value = attrib.type(a_value)
             attrs_kwargs[a_name] = a_value
         except KeyError:
-            logger.warn('Key not in source', key=a_name, source=source)
+            logger.warn("Key not in source", key=a_name, source=source)
 
     scheme = attrs(**attrs_kwargs)
     return scheme
 
 
-def generate_env_help(attrs, env_prefix=''):
+def generate_env_help(attrs, env_prefix=""):
     """Generate a list of all environment options."""
 
-    help = '\n'.join(       # noqa: W0622
-        '_'.join(
-            (env_prefix, ) + path if env_prefix else path
-        ).upper() for path, attrib in traverse_attrs(attrs)
+    help = "\n".join(  # noqa: W0622
+        "_".join((env_prefix,) + path if env_prefix else path).upper()
+        for path, attrib in traverse_attrs(attrs)
         if not isattrs(attrib)
     )
     return help
 
 
-def generate_toml_help(attrs, env_prefix='', parent=None):
+def generate_toml_help(attrs, env_prefix="", parent=None):
     if parent is None:
         parent = tomlkit.table()
-        doclines = trim(attrs.__doc__).split('\n')
+        doclines = trim(attrs.__doc__).split("\n")
         for line in doclines:
             parent.add(tomlkit.comment(line))
         parent.add(tomlkit.nl())
@@ -181,12 +190,12 @@ def generate_toml_help(attrs, env_prefix='', parent=None):
 
             if attrib.default not in (missing, attr.NOTHING):
                 default = (
-                    attrib.default() if callable(attrib.default)
-                    else attrib.default)
+                    attrib.default() if callable(attrib.default) else attrib.default
+                )
 
                 parent.add(attrib.name, default)
             else:
-                parent.add(tomlkit.comment(f'{attrib.name} ='))
+                parent.add(tomlkit.comment(f"{attrib.name} ="))
 
             parent.add(tomlkit.nl())
 
@@ -207,7 +216,7 @@ def traverse_attrs(attrs, with_nodes=False):
 def trim(docstring):
     # https://www.python.org/dev/peps/pep-0257/
     if not docstring:
-        return ''
+        return ""
     # Convert tabs to spaces (following the normal Python rules)
     # and split into a list of lines:
     lines = docstring.expandtabs().splitlines()
@@ -228,4 +237,4 @@ def trim(docstring):
     while trimmed and not trimmed[0]:
         trimmed.pop(0)
     # Return a single string:
-    return '\n'.join(trimmed)
+    return "\n".join(trimmed)
--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ def test_config_source_schematize(mocker):
 
     @attr.s(auto_attribs=True)
     class FooConfig:
-        bar: str = 'default'
+        bar: str = "default"
         foobar: float = 9.87
         baz: bool = config.bool_var(default=False)
 
@@ -25,86 +25,65 @@ def test_config_source_schematize(mocker):
         lst: typing.List = config.var(list)
 
     sources = [
+        {"bar": {"bim": "123.4", "foo": {"bar": "1.23", "baz": "true"}}},
+        {"foo": {"bar": "value", "foobar": 123.5, "baz": True}},
         {
-            'bar': {
-                'bim': '123.4',
-                'foo': {
-                    'bar': '1.23',
-                    'baz': 'true',
-                },
-            },
-        }, {
-            'foo': {
-                'bar': 'value',
-                'foobar': 123.5,
-                'baz': True,
-            }
-        }, {
-            'bar': {
-                'bim': '123.4',
-            },
-            'bim': {
-                'bar': {
-                    'bim': 1.23
-                },
-                'bam': 'on',
-                'lst': [1, 2, 3],
-            },
-        }
+            "bar": {"bim": "123.4"},
+            "bim": {"bar": {"bim": 1.23}, "bam": "on", "lst": [1, 2, 3]},
+        },
     ]
 
-    mocker.patch('os.environ', {
-        'PREFIX_BAR_BIM': '0',
-        'PREFIX_BAR_FOO_FOOBAR': '7.77',
-        'PREFIX_BAR_FOO_BAZ': 'false'
-    })
+    mocker.patch(
+        "os.environ",
+        {
+            "PREFIX_BAR_BIM": "0",
+            "PREFIX_BAR_FOO_FOOBAR": "7.77",
+            "PREFIX_BAR_FOO_BAZ": "false",
+        },
+    )
 
-    cfg = config.Config.from_sources(*sources, env_prefix='PREFIX')
-    cfg.bar = BarConfig
-    cfg.foo = FooConfig
-    cfg.bim = BimConfig
+    cfg = config.ConfigSource(*sources, env_prefix="PREFIX")
+    bar = cfg.load(BarConfig, "bar")
+    foo = cfg.load(FooConfig, "foo")
+    bim = cfg.load(BimConfig, "bim")
 
-    assert cfg.__dict__ == {
-        'bar': BarConfig(bim=0.0, foo=FooConfig(bar='1.23', foobar=7.77, baz=False)),
-        'foo': FooConfig(bar='value', foobar=123.5, baz=True),
-        'bim': BimConfig(bar=BarConfig(bim=1.23), bam=True, lst=[1, 2, 3]),
-    }
+    assert (bar, foo, bim) == (
+        BarConfig(bim=0.0, foo=FooConfig(bar="1.23", foobar=7.77, baz=False)),
+        FooConfig(bar="value", foobar=123.5, baz=True),
+        BimConfig(bar=BarConfig(bim=1.23), bam=True, lst=[1, 2, 3]),
+    )
 
-    assert cfg.foo.baz
+
+def test_load_general_config():
+    import attr
+    import typing
+    from buvar import config
+
+    sources = [{"foo": "bar", "group": {"some": "value"}}]
+    cfg = config.ConfigSource(*sources, env_prefix="PREFIX")
+
+    @attr.s(auto_attribs=True)
+    class GeneralVars:
+        foo: str
+        baz: typing.Optional[float] = None
+
+    general = cfg.load(GeneralVars)
+    assert general == GeneralVars("bar", None)
 
 
 def test_config_missing():
     import attr
     from buvar import config
 
-    source = {
-        'foo': {
-        }
-    }
+    source = {"foo": {}}
 
     @attr.s(auto_attribs=True)
     class FooConfig:
         bar: str = config.var()
 
-    cfg = config.Config.from_sources(source)
+    cfg = config.ConfigSource(source)
     with pytest.raises(ValueError):
-        cfg.foo = FooConfig
-
-
-def test_subclass():
-    import attr
-    from buvar import config
-
-    source = {
-        'bar': 'foobar',
-    }
-
-    @attr.s(auto_attribs=True)
-    class FooConfig(config.Config):
-        bar: str = config.var()
-
-    cfg = FooConfig.from_sources(source)
-    assert cfg == FooConfig(bar='foobar')
+        cfg.load(FooConfig, "foo")
 
 
 def test_generate_toml_help():
@@ -118,11 +97,12 @@ def test_generate_toml_help():
 
         bim bam
         """
-        string_val: str = config.var(help='string')
-        float_val: float = config.var(9.87, help='float')
-        bool_val: bool = config.bool_var(help='bool')
-        int_val: int = config.var(help='int')
-        list_val: typing.List = config.var(list, help='list')
+
+        string_val: str = config.var(help="string")
+        float_val: float = config.var(9.87, help="float")
+        bool_val: bool = config.bool_var(help="bool")
+        int_val: int = config.var(help="int")
+        list_val: typing.List = config.var(list, help="list")
 
     @attr.s(auto_attribs=True)
     class BarConfig:
@@ -131,22 +111,25 @@ def test_generate_toml_help():
         bla bla
         bli bli
         """
+
         bim: float
-        foo: FooConfig = config.var(help='foo')
+        foo: FooConfig = config.var(help="foo")
 
     env_vars = list(config.traverse_attrs(BarConfig))
 
     assert [path for path, _ in env_vars] == [
-        ('bim',),
-        ('foo', 'string_val'),
-        ('foo', 'float_val'),
-        ('foo', 'bool_val'),
-        ('foo', 'int_val'),
-        ('foo', 'list_val'),
+        ("bim",),
+        ("foo", "string_val"),
+        ("foo", "float_val"),
+        ("foo", "bool_val"),
+        ("foo", "int_val"),
+        ("foo", "list_val"),
     ]
 
-    help = config.generate_toml_help(BarConfig, env_prefix='PREFIX')
-    assert help.as_string() == """# BarConfig.
+    help = config.generate_toml_help(BarConfig, env_prefix="PREFIX")
+    assert (
+        help.as_string()
+        == """# BarConfig.
 # 
 # bla bla
 # bli bli
@@ -174,4 +157,5 @@ float_val = 9.87
 # list
 list_val = []
 
-""" # noqa: W291
+"""
+    )  # noqa: W291

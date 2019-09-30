d54beb8dec88d352b8bde869483167f529c833ea
Oliver Berger <oliver@digitalarchitekt.de>
2019-09-30 23:08:02 +0200

    add gitlab-ci

--- build_manylinux.sh
+++ build_manylinux.sh
@@ -15,17 +15,17 @@ EOF
 for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
     PIP=/opt/python/${PYTHON_VERSION}/bin/pip
     ${PIP} install cython
-    ${PIP} wheel /io[tests] -w /io/wheelhouse/
+    ${PIP} wheel .[tests] -w ./wheelhouse/
 done
 
 # repair wheels
-for WHL in /io/wheelhouse/*.whl; do
-    auditwheel repair "${WHL}" --plat ${PLATFORM} -w /io/wheelhouse/
+for WHL in ./wheelhouse/*.whl; do
+    auditwheel repair "${WHL}" --plat ${PLATFORM} -w ./wheelhouse/
 done
 
 # install and test
 for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
     BIN=/opt/python/${PYTHON_VERSION}/bin
-    "${BIN}/pip" install ${PROJECT}[tests] --no-index -f /io/wheelhouse
-    (cd /io/; ${BIN}/pytest tests)
+    "${BIN}/pip" install ${PROJECT}[tests] --no-index -f ./wheelhouse
+    ${BIN}/pytest tests
 done

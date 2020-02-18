955a89266869b78dd2c4714a9f560df41f29b5f1
Oliver Berger <oliver@digitalarchitekt.de>
2020-02-18 10:32:02 +0100

    Bump version: 0.20.2 → 0.20.3

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ entry_points = {"console_scripts": ["buvar = buvar.cli:main"]}
 
 setup_kwargs = {
     "name": "buvar",
-    "version": "0.20.2",
+    "version": "0.20.3",
     "description": "Asyncio plugins, components, dependency injection and configs",
     "long_description": None,
     "author": "Oliver Berger",
